4798a9551d4ae9406804c11ed1143179fbcb1957
Karlla Souza <karlladanielly94@hotmail.com>
2015-12-06 13:14:42 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,16 @@
+
+public class Pessoa {
+	static String nome;
+	static int idade;
+	static String cpf;
+	static String rg;
+	
+	Pessoa(String nome, int idade, String cpf, String rg){
+		Pessoa.nome=nome;
+		Pessoa.idade=idade;
+		Pessoa.cpf=cpf;
+		Pessoa.rg=rg;
+		
+	}
+
+}
