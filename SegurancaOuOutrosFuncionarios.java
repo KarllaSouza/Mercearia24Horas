bc12144d1871fbda6bc0336f016a952c823ad1cf
Karlla Souza <karlladanielly94@hotmail.com>
2015-12-06 13:51:02 -0300

    Create SegurancaOuOutrosFuncionarios.java

---
+++ SegurancaOuOutrosFuncionarios.java
@@ -0,0 +1,5 @@
+public class SegurancaOuOutrosFuncionarios extends Pessoa{
+	SegurancaOuOutrosFuncionarios(String nome,	int idade, String cpf, String rg, char turno){
+		super(nome, idade, cpf, rg, turno);
+	}
+}
