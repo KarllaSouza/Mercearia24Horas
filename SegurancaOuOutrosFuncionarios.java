fba54900039943600aba3f05c9ca444c0ef48689
Karlla Souza <karlladanielly94@hotmail.com>
2015-12-18 15:07:40 -0300

    Update SegurancaOuOutrosFuncionarios.java

--- SegurancaOuOutrosFuncionarios.java
+++ SegurancaOuOutrosFuncionarios.java
@@ -1,5 +1,5 @@
-public class SegurancaOuOutrosFuncionarios extends Pessoa{
-	SegurancaOuOutrosFuncionarios(String nome,	int idade, String cpf, String rg, char turno){
+public class SegurancaOuOutrosFuncionarios extends Funcionario{
+	SegurancaOuOutrosFuncionarios(String nome, int idade, String cpf, String rg, String turno){
 		super(nome, idade, cpf, rg, turno);
 	}
 }
