dc32457acca7b3db9ce5e1ce96bc8cecdc179a0a
Karlla Souza <karlladanielly94@hotmail.com>
2015-12-18 15:01:20 -0300

    Update GerenciaSistema.java

--- GerenciaSistema.java
+++ GerenciaSistema.java
@@ -4,9 +4,22 @@ public class GerenciaSistema {
 	static Scanner in = new Scanner(System.in);
 	
 	private static void opcaoGerente(){
-		//cadastrar Funcionários
-		//cadastrar Produtos
-		//Alterar estoque (adicionar ou retirar produtos)
+		System.out.println("Deseja realizar qual operação?\n  1. Cadastrar novo funcionário\n  2. Cadastrar novo produto\n  3. Alterar Estoque (adicionar ou remover itens)");
+		int operacao = in.nextInt();
+		switch (operacao){
+			case 1:	//cadastrar Funcionários
+				Gerente.cadastroDeNovosFuncionarios();
+				break;
+			case 2:	//cadastrar Produtos
+				
+				break;
+			case 3:	//Alterar estoque (adicionar ou retirar produtos)
+				
+				break;
+			default:
+				System.out.println("Opção Inválida!");
+		}
+		
 	}
 	private static void opcaoVendedor(){
 		//Alterar estoque (retirar produtos)
@@ -17,12 +30,10 @@ public class GerenciaSistema {
 		//funcionários noturnos recebem MAIS
 	}
 	
-
 	public static void main(String[] args) {
-	  System.out.println("*          * *    Mercearia JK     * *         *");
+		System.out.println("*          * *    Mercearia 24 Horas     * *         *");
 		System.out.println("Você é:\n  1. Gerente\n  2. Vendedor\n  3. Segurança/Outro Funcionário\n");
 		int func = in.nextInt();
-		
 		switch (func){
 			case 1:
 				opcaoGerente();
@@ -35,8 +46,6 @@ public class GerenciaSistema {
 				break;
 			default:
 				System.out.println("Opção Inválida.\nEste sistema é apenas para os funcionários da Mercearia");
-			
 		}
-		
 	}
 }
