550bc0166796d79aca38d179cca21a17e8afb7dc
Karlla Souza <karlladanielly94@hotmail.com>
2015-12-18 15:05:08 -0300

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,5 +1,41 @@
-public class Gerente extends Pessoa{
-	Gerente(String nome,	int idade, String cpf, String rg, char turno){
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Gerente extends Funcionario{
+	static Scanner in = new Scanner(System.in);
+	Gerente(String nome, int idade, String cpf, String rg, String turno){
 		super(nome, idade, cpf, rg, turno);
+	}
+	
+	static void cadastroDeNovosFuncionarios(){
+		Funcionario func = new Funcionario(nome, idade, cpf, rg, turno);
+		Calendar c = Calendar.getInstance();
+		Date data = c.getTime();
+		DateFormat f = DateFormat.getDateInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		func.dataQueFoiContratado = sdf.format(data);
+		
+		System.out.print("Nome do novo funcionário: ");
+		nome = in.nextLine();
+		System.out.print("Idade do novo funcionário: ");
+		idade = in.nextInt();
+		System.out.print("CPF do novo funcionário: ");
+		cpf = in.nextLine();
+		System.out.print("RG do novo funcionário: ");
+		rg = in.nextLine();
+		System.out.print("Turno do novo funcionário: ");
+		turno = in.nextLine();
+		
+		
+		System.out.println("\n\nContratação realizada com sucesso!\n");
+		System.out.print("Nome: "+func.nome+"      ");
+		System.out.print("Idade: "+func.idade+"      ");
+		System.out.print("CPF: "+func.cpf+"      ");
+		System.out.print("RG: "+func.rg+"      ");
+		System.out.print("Turno: "+func.turno+"      ");
+		System.out.println("\nData de contratação: "+func.dataQueFoiContratado/*sdf.format(data)*/+"\n");
 	}
 }
