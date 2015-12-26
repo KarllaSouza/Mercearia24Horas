ac69ebe883cf8a59615cf6655c24b3d4ed0312c1
Karlla Souza <karlladanielly94@hotmail.com>
2015-12-26 12:36:19 -0300

    Update Vendedor.java

--- Vendedor.java
+++ Vendedor.java
@@ -1,5 +1,8 @@
+
 public class Vendedor extends Funcionario{
-	Vendedor(String nome, int idade, String cpf, String rg, String turno){
-		super(nome, idade, cpf, rg, turno);
+	Vendedor(String nome,	int idade, String cpf, String rg, String turno,float salario){
+		super(nome, idade, cpf, rg, turno,salario);
+		isGerente = false;
+		salario=(float) 650.00;
 	}
 }
