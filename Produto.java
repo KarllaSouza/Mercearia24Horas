8ff2f41ad18417310578ef3410687d43bd129b02
Karlla Souza <karlladanielly94@hotmail.com>
2015-12-30 11:26:07 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Produto extends Estoque{
 	static Scanner in = new Scanner(System.in);
-	private static String nome;
-	private static int codigoDoProduto;
-	private static int quantidade;
-	private static double preco;
+	static String nome;
+	static int codigoDoProduto;
+	static int quantidade;
+	static double preco;
 	
 	public Produto(String nome, int codigoDoProduto, int quantidade, double preco) {
 		Produto.nome = nome;
