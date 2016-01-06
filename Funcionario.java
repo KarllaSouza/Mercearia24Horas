58d4312c9beacf7fcc2750c05a7033488b58f803
Karlla Souza <karlladanielly94@hotmail.com>
2016-01-05 22:27:00 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,36 +1,61 @@
-import java.util.Calendar;
-import java.text.DateFormat;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
+package DAO;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import BO.Produtos;
+import Modelo.GerenciaDoSistema;
+import Modelo.Palhaco;
+import Modelo.Seguranca;
+import view.CDUtils;
+
+
 
 public class Funcionario {
-	static String nome;
-	static int idade;
-	static Date dataNascimento;
-	static String dataQueFoiContratado;
-	static String cpf;
-	static String rg;
-	static String turno; //diurno ou noturno
-	static float salario;
-	static boolean isGerente = false;
-	Funcionario(String nome, int idade, String cpf, String rg, String turno, float salario){
-		Funcionario.nome=nome;
-		Funcionario.idade=idade;
-		Funcionario.cpf=cpf;
-		Funcionario.rg=rg;
-		Funcionario.turno=turno;
-		Funcionario.salario=salario;
+	
+	int id;
+	String nome;
+	static Scanner in = new Scanner(System.in);
+	static int opcao=90;
+	static Carrinho carrinho = new Carrinho();
+	
+	Funcionario(){
+		
 	}
-
-	public static void main(String[] args) throws ParseException {
-		Calendar c = Calendar.getInstance();
-		Date data = c.getTime();
-		DateFormat f = DateFormat.getDateInstance();
-		/*Date data2 = f.parse("12/01/1995");
-		System.out.println(data2);*/
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.println("Data de contratação: "+sdf.format(data));
-
+	
+	
+	public static void menuDoFuncionario(Connection conexao) throws SQLException{
+		Gerente.conexao = conexao;
+		System.out.println("Bem vindo Funcionario, qual operação deseja fazer?");
+			System.out.println("1 - Listar os produtos cadastrados.");
+			System.out.println("2 - Colocar produto no carrinho");
+			System.out.println("3 - Tirar item do carrinhho.");
+			System.out.println("4 - Finalizar Compra.");
+			System.out.println("5 - Chamar Segurança.");
+			System.out.println("6 - Chamar o palhaço para distrair as crianças.");
+			System.out.println("7 - Logout.");
+			opcao = in.nextInt();
+			switch(opcao){
+				case 1:
+					listarProdutos(2, conexao);
+					break;
+				case 2:
+					colocarNoCarrinho(conexao, 2);
+				case 3:
+					tirarItemDoCarrinho(conexao, 2);
+				case 4:
+					finalizarCompra(conexao, 2);
+				case 5:
+					Seguranca.SerChamado(conexao, 2);
+				case 6:
+					Palhaco.DivertirAsCriancas(conexao, 2);
+				case 7:
+					logOut();
+				default:
+					menuDoFuncionario(conexao);
+					break;
+			}
 	}
-}
