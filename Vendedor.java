d35c1e65d776dc80caecc8d44c0db4477e5eb514
pradinesjpr <pradinesjpr@gmail.com>
2016-01-05 22:10:53 -0300

    Delete Vendedor.java

--- Vendedor.java
+++
@@ -1,8 +0,0 @@
-
-public class Vendedor extends Funcionario{
-	Vendedor(String nome,	int idade, String cpf, String rg, String turno,float salario){
-		super(nome, idade, cpf, rg, turno,salario);
-		isGerente = false;
-		salario=(float) 650.00;
-	}
-}
