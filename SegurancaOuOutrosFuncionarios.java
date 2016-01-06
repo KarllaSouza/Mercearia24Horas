bd88766723367bfe58cb4642aaa48ac3ecdfdc1e
pradinesjpr <pradinesjpr@gmail.com>
2016-01-05 22:32:11 -0300

    Delete SegurancaOuOutrosFuncionarios.java

--- SegurancaOuOutrosFuncionarios.java
+++
@@ -1,7 +0,0 @@
-public class SegurancaOuOutrosFuncionarios extends Funcionario{
-	SegurancaOuOutrosFuncionarios(String nome, int idade, String cpf, String rg, String turno, float salario){
-		super(nome, idade, cpf, rg, turno, salario);
-		isGerente = false;
-		salario=(float) 650.00;
-	}
-}
