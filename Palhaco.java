8e5abeb36943c2e9a6b811fde30fdee564a62b18
Karlla Souza <karlladanielly94@hotmail.com>
2016-01-05 23:02:46 -0300

    Create Palhaco.java

---
+++ Palhaco.java
@@ -0,0 +1,19 @@
+package Modelo;
+
+import java.sql.SQLException;
+
+import DAO.Funcionario;
+import DAO.Gerente;
+
+public class Palhaco {
+	public static void DivertirAsCriancas(java.sql.Connection conexao, int menu) throws SQLException{
+		System.out.println("As Crianças estão distraidas, os pais podem ir fazer as compras.");
+		if(menu==1){
+			Gerente.menuDoGerente(conexao);
+		}
+		else{
+			Funcionario.menuDoFuncionario(conexao);
+		}
+		
+	}
+}
