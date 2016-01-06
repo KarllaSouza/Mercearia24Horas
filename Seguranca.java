13bb2c284e8850841682d79605bbac87668cfe8c
Karlla Souza <karlladanielly94@hotmail.com>
2016-01-05 23:03:38 -0300

    Update Seguranca.java

--- Seguranca.java
+++ Seguranca.java
@@ -1,21 +1,16 @@
 package Modelo;
 
 import java.sql.SQLException;
-
 import DAO.Funcionario;
 import DAO.Gerente;
-
-
 
 public class Seguranca {
 	public static void SerChamado(java.sql.Connection conexao, int menu) throws SQLException{
 		System.out.println("A caminho.");
 		if(menu==1){
 			Gerente.menuDoGerente(conexao);
-		}
-		else{
+		}else{
 			Funcionario.menuDoFuncionario(conexao);
 		}
-		
 	}
 }
